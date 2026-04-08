b6a30796e3b54afcf1b0707c482e7c7138dd8b2c
FAAAY <faytupoy@gmail.com>
2026-04-09 00:49:56 +0300

    Add moving right

--- ship.py
+++ ship.py
@@ -5,13 +5,20 @@ class Ship():
 
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию"""
-        self.screen= ai_game.screen
+        self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
+
+        self.moving_right = False
+
+    def update(self):
+        """Обновляет позицию корабля с учетом флага"""
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         """Рисует корабль в текущей позиции"""

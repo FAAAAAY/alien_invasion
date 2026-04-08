520e2158d8038cac2820cb8bffdc8c1d6ec19106
FAAAY <faytupoy@gmail.com>
2026-04-09 00:21:53 +0300

    Add ship on screen

--- ailen_invasion.py
+++ ailen_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AilenInvasion:
     """Класс для управления ресурсами и поведением игры"""
@@ -14,6 +15,7 @@ class AilenInvasion:
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Ailen Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -22,6 +24,7 @@ class AilenInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
 
 if __name__ == '__main__':

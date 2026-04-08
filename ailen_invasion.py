2b99283cbe8fa399cfbfbb5f786e5b9fef86a6a5
FAAAY <faytupoy@gmail.com>
2026-04-08 23:56:07 +0300

    New bg color

--- ailen_invasion.py
+++ ailen_invasion.py
@@ -11,6 +11,7 @@ class AilenInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Ailen Invasion")
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -18,7 +19,7 @@ class AilenInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
 
 if __name__ == '__main__':

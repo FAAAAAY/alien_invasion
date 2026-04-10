129e5b26db2b0d8eb21dacff07f9431d3f40b98a
FAAAY <faytupoy@gmail.com>
2026-04-10 10:31:54 +0300

    Add killing aliens

--- ailen_invasion.py
+++ ailen_invasion.py
@@ -112,6 +112,16 @@ class AilenInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+        
+    def _check_bullet_alien_collisions(self):
+        """Обработка коллизий снарядов с пришельцами"""
+
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)       
+
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()   
     def _update_aliens(self):
         """Обновляет позиции всех пришельцев во флоте"""
         self._check_fleet_edges()
